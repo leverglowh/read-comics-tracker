e77984f08e6910cc0fa868f926dfb4c63d44488b
leverglowh <leverglowh@gmail.com>
2021-02-13 04:38:15 +0100

    Add pagination to series page

--- src/entities/series/series.tsx
+++ src/entities/series/series.tsx
@@ -1,18 +1,39 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { connect } from 'react-redux';
 import { IRootState } from 'src/shared/reducers';
-import { getEntities as getSeriesList } from 'src/entities/series/series.reducer';
+import { getEntities as getSeriesList, reset as resetSeries } from 'src/entities/series/series.reducer';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 import Spinner from 'react-bootstrap/Spinner';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import SingleSeriesCard from './components/series-card';
 import './series.scss';
+import { IPaginationState, PaginationBar } from 'src/shared/util/components/pagination-bar';
 
 export interface ISeriesProps extends StateProps, DispatchProps {}
 
 const Series: React.FC<ISeriesProps> = (props) => {
+  const [pagination, setPagination] = useState<IPaginationState>({
+    currentPage: 0,
+    itemsCount: 0,
+  });
   const [searchText, setSearchText] = useState('');
+
+  /**
+   * Resets all fetched series data.
+   */
+  useEffect(() => () => {
+      props.resetSeries();
+    }
+  , []);
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+    setPagination({
+      ...pagination,
+      itemsCount: props.seriesCount,
+    })
+  }, [props.seriesList]);
 
   const handleSearchTextChange = (e) => {
     setSearchText(e.target.value);
@@ -29,10 +50,22 @@ const Series: React.FC<ISeriesProps> = (props) => {
     }
   };
 
+  const handlePageSelection = (newSelectedPage: number) => {
+    if (newSelectedPage !== pagination.currentPage && searchText) {
+      // selected new page, fetch data
+      props.getSeriesList(searchText, newSelectedPage);
+      setPagination({
+        ...pagination,
+        currentPage: newSelectedPage,
+      })
+    }
+  }
+
   return (
-    <div>
+    <div id="series-page-body">
       <div className='search-div'>
         <Form.Control
+          placeholder="Search series titles here.."
           onChange={handleSearchTextChange}
           autoComplete='off'
           value={searchText}
@@ -42,7 +75,7 @@ const Series: React.FC<ISeriesProps> = (props) => {
           <FontAwesomeIcon icon='search' />
         </Button>
       </div>
-      <div id="series-page-body">
+      <div id="series-page-comics-container">
         {props.loading && (
           <div className='loading-section'>
             <Spinner style={{ width: '2rem', height: '2rem' }} animation='grow' />
@@ -51,6 +84,7 @@ const Series: React.FC<ISeriesProps> = (props) => {
         {props.seriesList?.map((series) => (
           <SingleSeriesCard key={series.id} series={series} />
         ))}
+        <PaginationBar {...pagination} handlePageSelection={handlePageSelection}/>
       </div>
     </div>
   );
@@ -58,11 +92,13 @@ const Series: React.FC<ISeriesProps> = (props) => {
 
 const mapStateToProps = ({ series }: IRootState) => ({
   seriesList: series.entities,
+  seriesCount: series.totalCount,
   loading: series.loading,
 });
 
 const mapDispatchToProps = {
   getSeriesList,
+  resetSeries,
 };
 
 type StateProps = ReturnType<typeof mapStateToProps>;
---
+++ src/shared/util/components/pagination-bar.tsx
@@ -0,0 +1,124 @@
+import React from 'react';
+import Pagination from 'react-bootstrap/Pagination';
+
+export const ITEMS_PER_PAGE = 20;
+
+export interface IPaginationState {
+  currentPage: number;
+  itemsCount: number;
+}
+
+export interface IPaginationBarProps {
+  currentPage: number;
+  itemsCount: number;
+  location?: string;
+  handlePageSelection: (selectedPage: number) => void;
+}
+
+export const PaginationBar: React.FC<IPaginationBarProps> = (props) => {
+  const { currentPage, itemsCount, location } = props; // current page is 0 when page 1 is selected
+  const pagesCount = Math.ceil(itemsCount / ITEMS_PER_PAGE);
+  const handlePageSelection = (e) => {
+    e.persist();
+    let value = e.target.id.split('_')[1] || e.target.value;
+    if (value === 'prev') {
+      value = currentPage - 1;
+    } else if (value === 'next') {
+      value = currentPage + 1;
+    }
+    props.handlePageSelection(Number(value));
+  };
+
+  const itemsToDisplay = () => {
+    const items: any[] = [];
+    let prev: any = {};
+
+    for (let j = 0; j < pagesCount; j++) {
+      const item: any = {};
+      if (
+        j === 0 || // first page: always shown
+        j === pagesCount - 1 || // last page: always shown
+        j === currentPage - 1 || // previous page, shown
+        (currentPage === 0 && j === 1) || // page 1
+        (currentPage - 1 <= j && j <= currentPage + 1) // one previous and one after
+      ) {
+        item.display = 'display';
+      } else if (prev.display === 'disabled') {
+        item.display = 'hidden';
+      } else {
+        item.display = 'disabled';
+      }
+
+      items.push(item);
+      prev = { ...item };
+
+      if (item.display === 'hidden') {
+        prev.display = 'disabled';
+      }
+    }
+    return items;
+  };
+
+  return (
+    <div
+      style={{
+        marginTop: 10,
+        flex: '0 0 100%',
+        display: 'flex',
+        flexDirection: 'row',
+        flexWrap: 'wrap',
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}
+    >
+      <div>
+        Showing&nbsp;
+        {Math.min(currentPage * ITEMS_PER_PAGE + 1, itemsCount)} -&nbsp;
+        {Math.min(itemsCount, currentPage * ITEMS_PER_PAGE + ITEMS_PER_PAGE)} of {itemsCount} series&nbsp;
+      </div>
+      <div style={{ display: 'flex', flexDirection: 'row', justifyContent: 'center' }}>
+        <Pagination style={{ marginBottom: 0 }}>
+          <Pagination.Item
+            id='page_prev'
+            onClick={handlePageSelection}
+            disabled={currentPage <= 0}
+            href={location ? `${location}#${currentPage - 1}` : ''}
+          >
+            prev
+          </Pagination.Item>
+          {itemsToDisplay().map((item, i) =>
+            item.display === 'display' ? (
+              <Pagination.Item
+                active={i === currentPage}
+                key={i}
+                id={'page_' + i}
+                onClick={handlePageSelection}
+                href={location ? location + '#' : ''}
+              >
+                {i + 1}
+              </Pagination.Item>
+            ) : item.display === 'disabled' ? (
+              <Pagination.Item disabled key={i} id={'page_' + i} href={location ? location + '#' : ''}>
+                ...
+              </Pagination.Item>
+            ) : null
+          )}
+          <Pagination.Item id='page_next' onClick={handlePageSelection} disabled={currentPage >= pagesCount - 1}>
+            next
+          </Pagination.Item>
+        </Pagination>
+      </div>
+      <div>
+        &nbsp;
+        <label htmlFor="page-jump-select" style={{ marginBottom: 0 }}>Go to page&nbsp;</label>
+        <select id="page-jump-select" value={currentPage} onChange={handlePageSelection}>
+          {[...Array(pagesCount)].map((_, i) => (
+            <option value={i}>
+              {i + 1}
+            </option>
+          ))}
+        </select>
+      </div>
+    </div>
+  );
+};
